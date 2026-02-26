d79c7f15348a49b7c5936e18d3f3a376456088a7
Christian Gonzalez <christianleegonzalez9@gmail.com>
2026-02-26 12:21:30 -0800

    Adding user langauge type to languages var in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,15 @@
 import { AppNav } from "@/components/layout/AppNav";
 import { getUserLanguages } from "@/app/actions/languages";
 
+type UserLanguage = Awaited<ReturnType<typeof getUserLanguages>>[number];
+
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const userLanguages = await getUserLanguages();
-  const languages = userLanguages.map((ul) => ul.language);
+  const languages = userLanguages.map((ul : UserLanguage) => ul.language);
 
   return (
     <div className="flex h-screen">
